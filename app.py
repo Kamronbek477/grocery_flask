fd5cf3e87aeae4f87fe136bd50debbfb586502f9
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2023-04-05 17:58:03 +0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from db import GroceryDB
 
 
@@ -10,35 +10,99 @@ db = GroceryDB()
 @app.route('/grocery')
 def all_grocery():
     """Get all grocery"""
-    pass
+    fruits = db.all()
+
+    table = '<table border="1">'
+    table += """
+    <a href="http://127.0.0.1:5000/grocery"> All product </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/fruit"> fruit </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/vegetable"> vegetable </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/dairy"> dairy </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/bakery"> bakery </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/meat"> meat </a><br>
+    <a href="http://127.0.0.1:5000/grocery/type/grain"> grain </a><br>"""
+    table += """<tr><th>name</th> <th>quantity</th> <th>price</th> <th>type</th></tr>"""
+    for fruit in fruits:
+        table += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+    table += '</table>'
+    return table
 
 
 # view add grocery
 @app.route('/grocery/add', methods=['POST'])
 def add_grocery():
     """Add a grocery"""
-    pass
+    fruit = request.get_json(force=True)
+    db.add(fruit)
+    return {'status':200}
 
 
 # view all grocery by type
 @app.route('/grocery/type/<type>')
 def all_grocery_by_type(type):
     """Get all grocery by type"""
-    pass
+    fruits = db.get_by_type(type)
+    if fruits :
+        table = """
+        <table border="1">
+        <a href="http://127.0.0.1:5000/grocery"> All product </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/fruit"> fruit </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/vegetable"> vegetable </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/dairy"> dairy </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/bakery"> bakery </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/meat"> meat </a><br>
+        <a href="http://127.0.0.1:5000/grocery/type/grain"> grain </a><br>
+        <tr>
+            <th>name</th>
+            <th>quantity</th>
+            <th>price</th>
+            <th>type</th>
+        </tr>
+        """
+        for fruit in fruits:
+            table += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        table += """
+        </table>
+        """
 
+    return table
 
 # view all grocery by name
 @app.route('/grocery/name/<name>')
 def all_grocery_by_name(name):
     """Get all grocery by name"""
-    pass
+    fruits = db.get_by_name(name)
+    if fruits :
+        table = """
+        <table border="1">
+        <tr>
+            <th>name</th> 
+            <th>quantity</th> 
+            <th>price</th> 
+            <th>type</th>
+        </tr>
+        """
+        for fruit in fruits:
+            table += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        table += "</table>"
+
+    return table
 
 
 # view all grocery by price
 @app.route('/grocery/price/<float:price>')
 def all_grocery_by_price(price):
     """Get all grocery by price"""
-    pass
+    fruits = db.get_by_price(price)
+    if fruits :
+        table = """
+        <table border="1">
+        """
+        for fruit in fruits:
+            table += f'<tr><th>{fruit["name"]}</th><th>{fruit["quantity"]}</th><th>{fruit["price"]}$</th><th>{fruit["type"]}</th></tr>'
+        table += """</table>
+        """
+    return table
 
 
 
